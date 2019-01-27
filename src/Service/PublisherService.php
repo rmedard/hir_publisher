d13383a4489178bfc04a874bbccfede838022419
rmedard <medard.rebero@gmail.com>
2019-01-27 18:07:48 +0100

    W10

--- src/Service/PublisherService.php
+++ src/Service/PublisherService.php
@@ -11,10 +11,13 @@ namespace Drupal\hir_publisher\Service;
 
 use function count;
 use Drupal;
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\node\Entity\Node;
 
-class PublisherService {
+class PublisherService
+{
 
     protected $entityTypeManager;
 
@@ -23,33 +26,48 @@ class PublisherService {
      *
      * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
      */
-    public function __construct(EntityTypeManager $entityTypeManager) {
+    public function __construct(EntityTypeManager $entityTypeManager)
+    {
         $this->entityTypeManager = $entityTypeManager;
     }
 
-    public function loadExpiredAdverts($date){
-        $storage = $this->entityTypeManager->getStorage('node');
-        $query = $storage->getQuery()
-          ->condition('type', 'advert')
-          ->condition('status', Node::PUBLISHED)
-          ->condition('field_advert_expirydate', $date, '<');
-        $expired_adverts_ids = $query->execute();
-        if (isset($expired_adverts_ids) and count($expired_adverts_ids) > 0){
-            return $storage->loadMultiple($expired_adverts_ids);
-        } else {
-            Drupal::logger('hir_publisher')->debug('No expired adverts found');
-            return array();
+    public function loadExpiredAdverts($date)
+    {
+        try {
+            $storage = $this->entityTypeManager->getStorage('node');
+            $query = $storage->getQuery()
+                ->condition('type', 'advert')
+                ->condition('status', Node::PUBLISHED)
+                ->condition('field_advert_expirydate', $date, '<');
+            $expired_adverts_ids = $query->execute();
+            if (isset($expired_adverts_ids) and count($expired_adverts_ids) > 0) {
+                return $storage->loadMultiple($expired_adverts_ids);
+            } else {
+                Drupal::logger('hir_publisher')->debug('No expired adverts found');
+            }
+        } catch (InvalidPluginDefinitionException $e) {
+            Drupal::logger('hir_publisher')->error($e->getMessage());
+        } catch (PluginNotFoundException $e) {
+            Drupal::logger('hir_publisher')->error($e->getMessage());
         }
+        return array();
     }
 
-    public function loadNonMappedPropertyRequests() {
-        $storage = $this->entityTypeManager->getStorage('webform_submission');
-        $query = $storage->getQuery()
-            ->condition('webform_id', 'property_request_form')
-            ->addTag('is_pr_mapped');
-        $ids = $query->execute();
-        if (isset($ids) and count($ids) > 0) {
-            Drupal::logger('hir_publisher')->info('Ids: ' . json_encode($ids));
+    public function loadNonMappedPropertyRequests()
+    {
+        try {
+            $storage = $this->entityTypeManager->getStorage('webform_submission');
+            $query = $storage->getQuery()
+                ->condition('webform_id', 'property_request_form')
+                ->addTag('is_pr_mapped');
+            $ids = $query->execute();
+            if (isset($ids) and count($ids) > 0) {
+                Drupal::logger('hir_publisher')->info('Ids: ' . json_encode($ids));
+            }
+        } catch (InvalidPluginDefinitionException $e) {
+            Drupal::logger('hir_publisher')->error($e->getMessage());
+        } catch (PluginNotFoundException $e) {
+            Drupal::logger('hir_publisher')->error($e->getMessage());
         }
     }
 }
